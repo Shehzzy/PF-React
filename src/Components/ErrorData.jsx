ba59c2ca69780e935936dd6bcb7d9c6cd87b43f8
Shehzzy <111001914+Shehzzy@users.noreply.github.com>
2024-12-07 11:55:34 +0500

    Update ErrorData.jsx error img file

--- src/Components/ErrorData.jsx
+++ src/Components/ErrorData.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NoResults from '../assets/Error.png';
+import NoResults from '../assets/error.png';
 import { useNavigate } from "react-router-dom";
 
 function ErrorData() {
